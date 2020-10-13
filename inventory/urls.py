0dd01bfefd6be77c08b408d87e04cb99c6f41ce1
JagaranMaharjan <01jagaranmah10@gmail.com>
2020-10-13 12:36:45 +0545

    added features to add new devices in postgressql

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^display_laptop$', views.display_laptop, name="display_laptop"),
     url(r'^display_desktop$', views.display_desktop, name="display_desktop"),
     url(r'^display_mobile$', views.display_mobile, name="display_mobile"),
+    url(r'^add_laptop$', views.add_laptop, name="add_laptop"),
+    url(r'^add_desktop$', views.add_desktop, name="add_desktop"),
+    url(r'^add_mobile$', views.add_mobile, name="add_mobile"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,25 +1,60 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from .forms import *
 
 # Create your views here.
 from . import models
+from .forms import LaptopForm
 
 
 def index(request):
-    items = models.Desktop.objects.all()
-    return render(request, 'inventory/index.html', {"items": items})
+    return render(request, 'inventory/index.html')
 
 
 def display_laptop(request):
     items = models.Laptop.objects.all()
-    return render(request, 'inventory/index.html', {"items": items})
+    context = {
+        'items': items,
+        'header': "Laptops"
+    }
+    return render(request, 'inventory/index.html', context)
 
 
 def display_desktop(request):
     items = models.Desktop.objects.all()
-    return render(request, 'inventory/index.html', {"items": items})
+    context = {
+        'items': items,
+        'header': "Desktops"
+    }
+    return render(request, 'inventory/index.html', context)
 
 
 def display_mobile(request):
     items = models.Mobile.objects.all()
-    return render(request, 'inventory/index.html', {"items": items})
+    context = {
+        'items': items,
+        'header': "Mobiles"
+    }
+    return render(request, 'inventory/index.html', context)
+
+
+def add_device(request, cls):
+    if request.method == "POST":
+        form = cls(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = cls()
+        return render(request, 'inventory/add_new.html', {'form': form})
+
+
+def add_laptop(request):
+    return add_device(request, LaptopForm)
+
+
+def add_desktop(request):
+    return add_device(request, DesktopForm)
+
+
+def add_mobile(request):
+    return add_device(request, MobileForm)
