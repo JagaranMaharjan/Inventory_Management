f2b9534cba0a27ce261081d433617ae058af3c3e
JagaranMaharjan <01jagaranmah10@gmail.com>
2020-10-15 16:41:27 +0545

    added edit, update and delete features.

--- inventory/urls.py
+++ inventory/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     url(r'^add_laptop$', views.add_laptop, name="add_laptop"),
     url(r'^add_desktop$', views.add_desktop, name="add_desktop"),
     url(r'^add_mobile$', views.add_mobile, name="add_mobile"),
+    url(r'^edit_laptop/(?P<pk>\d+)$', views.edit_laptop, name="edit_laptop"),
+    url(r'^edit_desktop/(?P<pk>\d+)$', views.edit_desktop, name="edit_desktop"),
+    url(r'^edit_mobile/(?P<pk>\d+)$', views.edit_mobile, name="edit_mobile"),
+    url(r'^delete_laptop/(?P<pk>\d+)$', views.delete_laptop, name="delete_laptop"),
+    url(r'^delete_desktop/(?P<pk>\d+)$', views.delete_desktop, name="delete_desktop"),
+    url(r'^delete_mobile/(?P<pk>\d+)$', views.delete_mobile, name="delete_mobile"),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import *
 
 # Create your views here.
@@ -58,3 +58,57 @@ def add_desktop(request):
 
 def add_mobile(request):
     return add_device(request, MobileForm)
+
+
+def edit_device(request, pk, model, form_name):
+    item = get_object_or_404(model, pk=pk)
+    if request.method == "POST":
+        form = form_name(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = form_name(instance=item)
+        return render(request, 'inventory/edit_item.html', {'form': form})
+
+
+def edit_laptop(request, pk):
+    return edit_device(request, pk, Laptop, LaptopForm)
+
+
+def edit_desktop(request, pk):
+    return edit_device(request, pk, Desktop, DesktopForm)
+
+
+def edit_mobile(request, pk):
+    return edit_device(request, pk, Mobile, MobileForm)
+
+
+def delete_laptop(request, pk):
+    Laptop.objects.filter(id=pk).delete()
+    items = models.Laptop.objects.all()
+    context = {
+        'items': items,
+        'header': "Laptops"
+    }
+    return render(request, 'inventory/index.html', context)
+
+
+def delete_desktop(request, pk):
+    Desktop.objects.filter(id=pk).delete()
+    items = models.Desktop.objects.all()
+    context = {
+        'items': items,
+        'header': "Desktops"
+    }
+    return render(request, 'inventory/index.html', context)
+
+
+def delete_mobile(request, pk):
+    Mobile.objects.filter(id=pk).delete()
+    items = models.Mobile.objects.all()
+    context = {
+        'items': items,
+        'header': "Mobiles"
+    }
+    return render(request, 'inventory/index.html', context)
